8d50607de1c65d7c55bbed6e940bc83327174cd5
Rizky Eko <rizky.eko@elephanttalk.com>
2017-01-26 21:50:51 +0800

    Add while loop

--- NumberToText/NumberToText/main.cpp
+++ NumberToText/NumberToText/main.cpp
@@ -10,48 +10,56 @@ int main()
 	long long int num;
 	string stringLang;
 	int lang;
+	char flag;
 
 	cout << "===Number to Word Converter===" << endl << endl;
-	cout << "Enter a Number: ";
-	cin >> num;
-	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	do {
 
-	while (cin.fail()) {
-		cin.clear();
-		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		cout << "Please Enter a Valid Number: ";
+		cout << "Enter a Number: ";
 		cin >> num;
 		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	}
 
-	cout << "----------------------" << endl;
-	cout << "Language Option" << endl;
-	cout << "----------------------" << endl;
-	cout << "0. " << "English (default)" << endl;
-	cout << "1. " << "Indonesia" << endl;
-	cout << "2. " << "Vietnam" << endl;
-	cout << "----------------------" << endl;
+		while (cin.fail()) {
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			cout << "Please Enter a Valid Number: ";
+			cin >> num;
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
 
-	cout << "Enter output Language: ";
-	getline(cin, stringLang);
-	if (stringLang == "") {
-		lang = 0;
-	}
-	else {
-		lang = std::stoi(stringLang);
-	}
+		cout << "----------------------" << endl;
+		cout << "Language Option" << endl;
+		cout << "----------------------" << endl;
+		cout << "0. " << "English (default)" << endl;
+		cout << "1. " << "Indonesia" << endl;
+		cout << "2. " << "Vietnam" << endl;
+		cout << "----------------------" << endl;
 
-	IntToWord number_1(num, lang);
-	long long int numbers = number_1.getNum();
-	int languages = number_1.getLang();
-	
-	// if other language not yet developed
-	if (lang != 0) {
-		cout << endl << "Language not yet developed" << endl << endl;
-	}
-	else {
-		cout << endl << "The number you enter is: " << number_1.convert_number(numbers, languages) << endl << endl;
-	}
+		cout << "Enter output Language: ";
+		getline(cin, stringLang);
+		if (stringLang == "") {
+			lang = 0;
+		}
+		else {
+			lang = std::stoi(stringLang);
+		}
+
+		IntToWord number_1(num, lang);
+		long long int numbers = number_1.getNum();
+		int languages = number_1.getLang();
+
+		// if other language not yet developed
+		if (lang != 0) {
+			cout << endl << "Language not yet developed" << endl << endl;
+		}
+		else {
+			cout << endl << "The number you enter is: " << number_1.convert_number(numbers, languages) << endl << endl;
+		}
+
+		cout << endl << "Want to convert more number ? (Y / N) ";
+		cin >> flag;
+		cout << endl << endl;
+	} while (flag == 'Y' || flag == 'y');
 
 	system("PAUSE");
 	return 0;
